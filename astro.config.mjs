36293c7a9d58be28aaad4f7d28ce6172fe6bc96a
Roger <myvarequals@gmail.com>
2026-04-21 04:33:23 -0400

    added netlify adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,36 +5,35 @@ import tailwindcss from '@tailwindcss/vite';
 
 import preact from '@astrojs/preact';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
-
-    fonts: [
-      {
-        provider: fontProviders.fontsource(),
-        name: 'Permanent Marker',
-        cssVariable: "--permanent-marker",
-      },
-      {
-        provider: fontProviders.fontsource(),
-        name: 'Varela Round',
-        cssVariable: "--varela-round",
-      },
-      {
-        provider: fontProviders.fontsource(),
-        name: 'Noto Sans',
-        cssVariable: "--noto-sans",
-      }
-    ],
-
-    integrations: [preact()],
-
-    output: "server",
-
-    site: "https://2026-webcrafter.netlify.app",
-
-    vite: {
-      plugins: [tailwindcss()]
+  fonts: [
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Permanent Marker',
+      cssVariable: "--permanent-marker",
     },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Varela Round',
+      cssVariable: "--varela-round",
+    },
+    {
+      provider: fontProviders.fontsource(),
+      name: 'Noto Sans',
+      cssVariable: "--noto-sans",
+    }
+  ],
 
-  
+  integrations: [preact()],
+  output: "server",
+  site: "https://2026-webcrafter.netlify.app",
+
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  adapter: netlify(),
 });
