c4bdc31d750d264c2b65e4722b6a992396dd9b2b
Roger <myvarequals@gmail.com>
2026-05-25 19:53:23 -0400

    fixing toggle animation for faq accordion

--- src/scripts/toggle.js
+++ src/scripts/toggle.js
@@ -1,15 +1,19 @@
 const toggle = document.querySelectorAll('.toggle-header');
 
-// toggle.map((t) => {
-//     t.addEventListener('click', () => {
-//         const isExpanded = t.getAttribute('aria-expanded') === 'true';
-//         t.setAttribute('aria-expanded', `${!isExpanded}`);
-//     });
-// });
-
 toggle.forEach((t) => {
     t.addEventListener('click', () => {
+
         let isExpanded = t.getAttribute('aria-expanded') === 'true';
         t.setAttribute('aria-expanded', `${!isExpanded}`);
+        
+        let panel = t.nextElementSibling;
+
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            console.log(panel.scrollHeight);
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+        
     });
 });
